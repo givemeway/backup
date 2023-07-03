62ce67e2681a5952599a7ad3884688fe3edcf934
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2023-07-03 17:17:30 +0530

    saving local changes

---
+++ frontend/resources/index.js
@@ -0,0 +1,19 @@
+document.getElementById("loginform").addEventListener("submit", login);
+
+function login(event) {
+  let elements = event.target.elements;
+  const username = elements.email.value;
+  const password = elements.password.value;
+  const url = "http://192.168.29.34:3000/app/receiveFile";
+  const headers = {
+    Authorization: btoa(`${username}:${password}`),
+  };
+  const options = {
+    headers: headers,
+    method: "POST",
+  };
+  fetch(url, options)
+    .then((res) => res.json())
+    .then((data) => console.log(data))
+    .catch((err) => console.log(err));
+}
