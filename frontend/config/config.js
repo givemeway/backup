74669b216084865d8368eba4a685d80fc0e33810
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2023-08-07 15:16:22 +0530

    Updated Download files route to dynamically receive the file information

--- frontend/config/config.js
+++ frontend/config/config.js
@@ -1,4 +1,4 @@
-const host = "https://172.26.96.1:3001";
+const host = "https://localhost:3001";
 const fileUploadURL = `${host}/app/receiveFiles`;
 const loginURL = `${host}/app/login`;
 const fetchFilesURL = `${host}/app/getCurrentDirFiles`;
