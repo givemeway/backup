043333d0a2ab708e868dc07ea642f47a55c28152
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2024-01-19 09:01:45 +0530

    replaced hard coded url with constant

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,7 +1,12 @@
 const domain = "localhost";
 const host_url = `http://${domain}`;
 const origin = `${host_url}:3000`;
+<<<<<<< HEAD
 const serverDomain = `https://localhost:3001`;
+=======
+const serverDomain = `http://localhost:3001`;
+const frontEndDomain = `http://localhost:3000`;
+>>>>>>> 99205bb (replaced hard coded url with constant)
 // const domain = "wealthy-grouse-modest.ngrok-free.app";
 // const host_url = `https://wealthy-grouse-modest.ngrok-free.app`;
 // const origin = `https://wealthy-grouse-modest.ngrok-free.app`;
@@ -9,4 +14,4 @@ const serverDomain = `https://localhost:3001`;
 
 // https://wealthy-grouse-modest.ngrok-free.app/
 //  "ngrok http --host-header=rewrite --domain=wealthy-grouse-modest.ngrok-free.app 3000"
-export { origin, domain, serverDomain };
+export { origin, domain, serverDomain, frontEndDomain };
