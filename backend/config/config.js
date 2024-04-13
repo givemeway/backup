e89b9239c8b7a7836d9f7352343b6322ad076deb
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2024-04-13 21:55:18 +0530

    added the domain as https://qdrive.netlify.app

--- backend/config/config.js
+++ backend/config/config.js
@@ -7,7 +7,7 @@ dotenv.config();
 // export const serverDomain = `http://localhost:3001`;
 // export const frontEndDomain = `http://localhost:3000`;
 // export const thumbnailMicroservice = "http://localhost:3003/api/v1/preview";
-export const domain = "qdrive.netlify.app";
+export const domain = "https://qdrive.netlify.app";
 export const host_url = `http://${domain}`;
 export const origin = "https://qdrive.netlify.app";
 export const serverDomain = "https://backup-8toq.onrender.com";
@@ -34,6 +34,7 @@ export const cookieOpts = {
 export const csrfCookieOpts = {
   secure: true,
   httpOnly: false,
+  sameSite: "none",
   path: "/",
   domain: domain,
 };
