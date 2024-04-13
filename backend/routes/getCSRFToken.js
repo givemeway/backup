1e345edce721b94fc7bdeaae5691af12f5be3721
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2024-04-13 19:25:58 +0530

    added set-cookied for _csrf token

--- backend/routes/getCSRFToken.js
+++ backend/routes/getCSRFToken.js
@@ -1,8 +1,22 @@
 import express from "express";
+import cookie from "cookie";
+import { domain } from "../config/config";
 const router = express.Router();
 
 router.get("/", (req, res) => {
-  res.status(200).json({ CSRFToken: req.csrfToken() });
+  const CSRFToken = req.csrfToken();
+  res.setHeader(
+    "Set-Cookie",
+    cookie.serialize("_csrf", CSRFToken, {
+      secure: true,
+      httpOnly: true,
+      sameSite: "none",
+      path: "/",
+      domain: domain,
+      expires: new Date(Date.now() + 86400),
+    })
+  );
+  res.status(200).json({ CSRFToken });
 });
 
 export { router as csrftoken };
