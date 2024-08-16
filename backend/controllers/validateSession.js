7b1a46e8d53c0713f2722fc8084216079f9b840b
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2024-08-16 20:56:41 +0530

    modified verify session to fetch the user details along with the avatar information

--- backend/controllers/validateSession.js
+++ backend/controllers/validateSession.js
@@ -1,10 +1,26 @@
-export const validateSession = (req, res) => {
-  res.status(200).json({
-    success: true,
-    userID: req.user.userID,
-    email: req.user.email,
-    username: req.user.Username,
-    first: req.user.first,
-    last: req.user.last,
-  });
+import { createHash } from "crypto";
+import { Avatar } from "../models/mongodb.js";
+import { getSignedURL } from "./createSignedURL.js";
+
+export const validateSession = async (req, res) => {
+  try {
+    const user = await Avatar.findOne({ username: req.user.Username });
+    const username = req.user.Username;
+    const id = createHash("sha1").update(username).digest("hex");
+    const url = await getSignedURL(id, username, true, ["32w"]);
+    res.status(200).json({
+      success: true,
+      userID: req.user.userID,
+      email: req.user.email,
+      username: user.username,
+      first: user.firstName,
+      last: user.lastName,
+      initials: user.initial,
+      avatar_url: url["32w"],
+      hasAvatar: user.has_avatar,
+    });
+  } catch (err) {
+    console.log(err);
+    res.status(500).json({ success: false, msg: err });
+  }
 };
