09cf62e18f55a4e5e43757bcefee3e811ca1a8e0
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2024-08-20 23:57:37 +0530

    added changes to enable 2FA in DB

--- backend/controllers/verifyOTP.js
+++ backend/controllers/verifyOTP.js
@@ -1,7 +1,8 @@
 import { OTP_EXPIRY } from "../config/config.js";
 import { prismaUser as prisma } from "../config/prismaDBConfig.js";
-import { hotp } from "otplib";
-// hotp.options = { step: 60 * 5 };
+import { hotp, totp } from "otplib";
+import { authenticator } from "@otplib/preset-default";
+import base32Encode from "base32-encode";
 
 export const fn_verifyOTP = (username, token) => {
   return new Promise(async (resolve, reject) => {
@@ -14,7 +15,6 @@ export const fn_verifyOTP = (username, token) => {
       if (user) {
         const now = Date.now();
         const diff = (now - parseInt(user.OTPGenTime)) / 1000;
-        console.log(now, parseInt(user.OTPGenTime), diff);
         if (diff < OTP_EXPIRY) {
           const isValid = hotp.verify({
             token: token,
@@ -31,8 +31,10 @@ export const fn_verifyOTP = (username, token) => {
                 OTPGenTime: Date.now(),
               },
             });
+            resolve(isValid);
+          } else {
+            reject("Invalid Code!");
           }
-          resolve(isValid);
         } else {
           reject(`Code Expired!`);
         }
@@ -45,18 +47,72 @@ export const fn_verifyOTP = (username, token) => {
   });
 };
 
+export const verifyTOTP = (username, token) => {
+  return new Promise(async (resolve, reject) => {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          username,
+        },
+      });
+      if (user) {
+        const unsignedArrBuff = Buffer.from(user.enc.toUpperCase(), "hex");
+        const base32_secret = base32Encode(unsignedArrBuff, "RFC4648", {
+          padding: false,
+        });
+        const isValid = authenticator.check(token, base32_secret);
+        if (isValid) resolve(isValid);
+        else reject("Invalid Code!");
+      } else {
+        reject(`${username} not found`);
+      }
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
 export const verifyOTP = async (req, res, next) => {
   try {
     const username = req.user.Username;
-    const { token, mfa } = req.query;
-    console.log(token, mfa);
+    const { token, mfa, is2FAConfig } = req.query;
     if (mfa === "email" || mfa === "sms") {
       const isValid = await fn_verifyOTP(username, token);
-      console.log(token, isValid);
+      if (is2FAConfig && isValid) {
+        await prisma.user.update({
+          where: {
+            username,
+          },
+          data: {
+            is2FA: true,
+            isEmail: true,
+            isTOTP: false,
+            isSMS: false,
+          },
+        });
+      }
+
       res
         .status(200)
         .json({ success: true, msg: `OTP validity is ${isValid}`, isValid });
     } else if (mfa === "totp") {
+      const isValid = await verifyTOTP(username, token);
+      if (is2FAConfig && isValid) {
+        await prisma.user.update({
+          where: {
+            username,
+          },
+          data: {
+            is2FA: true,
+            isEmail: false,
+            isTOTP: true,
+            isSMS: false,
+          },
+        });
+      }
+      res
+        .status(200)
+        .json({ success: true, msg: `OTP validity is ${isValid}`, isValid });
     }
   } catch (err) {
     console.log(err);
