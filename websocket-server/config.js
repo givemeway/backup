102e41f44190bd97adf9d934838fec48121c12d6
Sandeep Kumar G R <sand.kumar.gr@gmail.com>
2026-05-12 16:05:38 +0530

    Update CORS options to include additional origin

--- websocket-server/config.js
+++ websocket-server/config.js
@@ -1,6 +1,6 @@
 export const ORIGIN = "https://websocket-server-v220.onrender.com";
 export const corsOpts = {
-  origin: ORIGIN,
+  origin: [ORIGIN, "https://qdrive.online"],
   allowedHeaders:
     "Content-Type,X-CSRF-Token,Authorization,Origin,filename,dir,devicename,Content-Disposition,filestat,currentdirectory,backuptype",
   exposedHeaders: "Set-Cookie",
